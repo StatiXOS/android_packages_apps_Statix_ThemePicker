9740abe70520f0ce2c890fe1e2b0e2c9d1734e78
Anay Wadhera <anay1018@gmail.com>
2023-03-13 23:44:44 -0400

    StatixThemePicker: update injections to qpr2
    * clean up
    
    Change-Id: I526ecf69fa6841932095f20b0fdec107920f9ccc

--- src/com/statix/android/customization/module/StatixCustomizationSections.java
+++ src/com/statix/android/customization/module/StatixCustomizationSections.java
@@ -1,18 +1,14 @@
 package com.statix.android.customization.module;
 
-import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ViewModelProvider;
 
-import com.android.customization.model.color.ColorSectionController;
-import com.android.customization.model.grid.GridOptionsManager;
-import com.android.customization.model.grid.GridSectionController;
-import com.android.customization.model.mode.DarkModeSectionController;
-import com.android.customization.model.theme.OverlayManagerCompat;
-import com.android.customization.model.themedicon.ThemedIconSectionController;
-import com.android.customization.model.themedicon.ThemedIconSwitchProvider;
+import com.android.customization.picker.quickaffordance.domain.interactor.KeyguardQuickAffordancePickerInteractor;
+import com.android.customization.picker.quickaffordance.ui.viewmodel.KeyguardQuickAffordancePickerViewModel;
 import com.android.wallpaper.model.CustomizationSectionController;
 import com.android.wallpaper.model.CustomizationSectionController.CustomizationSectionNavigationController;
 import com.android.wallpaper.model.PermissionRequester;
@@ -20,7 +16,10 @@ import com.android.wallpaper.model.WallpaperColorsViewModel;
 import com.android.wallpaper.model.WallpaperPreviewNavigator;
 import com.android.wallpaper.model.WallpaperSectionController;
 import com.android.wallpaper.model.WorkspaceViewModel;
+import com.android.wallpaper.module.CurrentWallpaperInfoFactory;
 import com.android.wallpaper.module.CustomizationSections;
+import com.android.wallpaper.picker.customization.ui.section.ScreenPreviewSectionController;
+import com.android.wallpaper.util.DisplayUtils;
 
 import com.statix.android.customization.model.font.FontManager;
 import com.statix.android.customization.model.font.FontSectionController;
@@ -31,40 +30,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 /** {@link CustomizationSections} for the customization picker. */
-public final class StatixCustomizationSections implements CustomizationSections {
+public final class StatixCustomizationSections extends DefaultCustomizationSections {
+
+    public StatixCustomizationSections(
+            KeyguardQuickAffordancePickerInteractor keyguardQuickAffordancePickerInteractor,
+            KeyguardQuickAffordancePickerViewModel.Factory
+                    keyguardQuickAffordancePickerViewModelFactory) {
+        super(keyguardQuickAffordancePickerInteractor, keyguardQuickAffordancePickerViewModelFactory);
+    }
 
     @Override
-    public List<CustomizationSectionController<?>> getAllSectionControllers(Activity activity,
-            LifecycleOwner lifecycleOwner, WallpaperColorsViewModel wallpaperColorsViewModel,
-            WorkspaceViewModel workspaceViewModel, PermissionRequester permissionRequester,
+    public List<CustomizationSectionController<?>> getSectionControllersForScreen(
+            Screen screen,
+            FragmentActivity activity,
+            LifecycleOwner lifecycleOwner,
+            WallpaperColorsViewModel wallpaperColorsViewModel,
+            WorkspaceViewModel workspaceViewModel,
+            PermissionRequester permissionRequester,
             WallpaperPreviewNavigator wallpaperPreviewNavigator,
             CustomizationSectionNavigationController sectionNavigationController,
-            @Nullable Bundle savedInstanceState) {
-        List<CustomizationSectionController<?>> sectionControllers = new ArrayList<>();
-
-        // Wallpaper section.
-        sectionControllers.add(new WallpaperSectionController(
-                activity, lifecycleOwner, permissionRequester, wallpaperColorsViewModel,
-                workspaceViewModel, sectionNavigationController, wallpaperPreviewNavigator,
-                savedInstanceState));
-
-        // Color section
-        sectionControllers.add(
-            new ColorSectionController(activity, wallpaperColorsViewModel, lifecycleOwner,
-                savedInstanceState));
-
-        // Dark/Light theme section.
-        sectionControllers.add(new DarkModeSectionController(activity,
-                lifecycleOwner.getLifecycle()));
-
-        // Themed app icon section.
-        sectionControllers.add(new ThemedIconSectionController(
-                ThemedIconSwitchProvider.getInstance(activity), workspaceViewModel,
-                savedInstanceState));
-
-        // App grid section.
-        sectionControllers.add(new GridSectionController(
-                GridOptionsManager.getInstance(activity), sectionNavigationController));
+            @Nullable Bundle savedInstanceState,
+            CurrentWallpaperInfoFactory wallpaperInfoFactory,
+            DisplayUtils displayUtils) {
+        List<CustomizationSectionController<?>> sectionControllers =
+                super.getSectionControllersForScreen(screen, activity, lifecycleOwner, wallpaperColorsViewModel, workspaceViewModel,
+                permissionRequester, wallpaperPreviewNavigator, sectionNavigationController, savedInstanceState, wallpaperInfoFactory,
+                displayUtils);
 
         // Icon pack selection section.
         sectionControllers.add(new IconPackSectionController(
@@ -73,6 +64,33 @@ public final class StatixCustomizationSections implements CustomizationSections 
         // Font selection section.
         sectionControllers.add(new FontSectionController(
                 FontManager.getInstance(activity, new OverlayManagerCompat(activity)), sectionNavigationController));
+
+        return sectionControllers;
+    }
+
+    @Override
+    public List<CustomizationSectionController<?>> getAllSectionControllers(
+            FragmentActivity activity,
+            LifecycleOwner lifecycleOwner,
+            WallpaperColorsViewModel wallpaperColorsViewModel,
+            WorkspaceViewModel workspaceViewModel,
+            PermissionRequester permissionRequester,
+            WallpaperPreviewNavigator wallpaperPreviewNavigator,
+            CustomizationSectionNavigationController sectionNavigationController,
+            @Nullable Bundle savedInstanceState,
+            DisplayUtils displayUtils) {
+        List<CustomizationSectionController<?>> sectionControllers =
+                super.getAllSectionControllers(activity, lifecycleOwner, wallpaperColorsViewModel, workspaceViewModel, permissionRequester,
+                wallpaperPreviewNavigator, sectionNavigationController, savedInstanceState, wallpaperInfoFactory, displayUtils);
+
+        // Icon pack selection section.
+        sectionControllers.add(new IconPackSectionController(
+                IconPackManager.getInstance(activity, new OverlayManagerCompat(activity)), sectionNavigationController));
+
+        // Font selection section.
+        sectionControllers.add(new FontSectionController(
+                FontManager.getInstance(activity, new OverlayManagerCompat(activity)), sectionNavigationController));
+
         return sectionControllers;
     }
 }
