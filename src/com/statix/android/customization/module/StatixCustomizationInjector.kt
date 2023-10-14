3f689449b62727062d428b58c4bb89c644cd5905
Anay Wadhera <anay1018@gmail.com>
2023-10-14 19:37:18 -0400

    StatixThemePicker: fixup for U
    Change-Id: I7c9f961b852d51daf7a73b9da732bf5256d598fa

--- src/com/statix/android/customization/module/StatixCustomizationInjector.kt
+++ src/com/statix/android/customization/module/StatixCustomizationInjector.kt
@@ -9,7 +9,7 @@ import com.android.wallpaper.model.WallpaperInfo
 import com.android.wallpaper.module.CustomizationSections
 import com.android.wallpaper.picker.MonetPreviewFragment
 
-public class StatixCustomizationInjector : ThemePickerInjector() {
+class StatixCustomizationInjector : ThemePickerInjector() {
 
   private var customizationSections: CustomizationSections? = null
 
@@ -34,9 +34,7 @@ public class StatixCustomizationInjector : ThemePickerInjector() {
                 ),
                 getKeyguardQuickAffordancePickerInteractor(activity),
                 getKeyguardQuickAffordancePickerViewModelFactory(activity),
-                NotificationSectionViewModel.Factory(
-                    interactor = getNotificationsInteractor(activity),
-                ),
+                getNotificationSectionViewModelFactory(activity),
                 getFlags(),
                 getClockCarouselViewModelFactory(
                     getClockPickerInteractor(activity.applicationContext),
